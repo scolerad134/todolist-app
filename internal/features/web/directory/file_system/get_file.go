13ba6c1d4c01a5899d6756e44633e0337496fb1b
scolerad134 <gugac134@gmail.com>
2026-05-15 20:06:46 +0300

    fix error to path '/'

--- internal/features/web/directory/file_system/get_file.go
+++ internal/features/web/directory/file_system/get_file.go
@@ -4,13 +4,15 @@ import (
 	"errors"
 	"fmt"
 	"os"
+
+	core_errors "github.com/scolerad134/todolist-app/internal/core/errors"
 )
 
 func (r *WebRepository) GetFile(filePath string) ([]byte, error) {
 	file, err := os.ReadFile(filePath)
 	if err != nil {
 		if errors.Is(err, os.ErrNotExist) {
-			return nil, fmt.Errorf("file: %s: %w", filePath, err)
+			return nil, fmt.Errorf("file: %s: %w", filePath, core_errors.ErrNotFound)
 		}
 
 		return nil, fmt.Errorf("get file: %s: %w", filePath, err)
